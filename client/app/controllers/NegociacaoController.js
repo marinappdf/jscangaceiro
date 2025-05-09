1edc477220a8c215b6a967e7876aa63e15ebd6b4
Marina <marinappdf@gmail.com>
2025-05-09 17:02:08 -0300

    Capítulo 4

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -9,34 +9,26 @@ class NegociacaoController {
   }
 
   adicionar(event) {
-    // Previne o comportamento padrão do evento, impedir a submissão do formulário
     event.preventDefault();
 
-    // let $ = document.querySelector.bind(document);
-    // let inputData = $("#data");
-    // let inputQuantidade = $("#quantidade");
-    // let inputValor = $("#valor");
-    // console.log(this._inputData.value);
-    // console.log(parseInt(this._inputQuantidade.value));
-    // console.log(parseFloat(this._inputValor.value));
-    //alert("Formulário enviado com sucesso pelo controller!");
+    // let converter = new DataConverter();
+    // let data = converter.paraData(this._inputData.value);
+    // let negociacao = new Negociacao(
+    //   data,
+    //   parseInt(this._inputQuantidade.value),
+    //   parseFloat(this._inputValor.value)
+    // );
+    // let diaMesAno = converter.paraTexto(negociacao.data);
+    // console.log(diaMesAno);
 
-    // agora o Date recebe um array
-    let data = new Date(
-      ...this._inputData.value.split("-").map((item, indice) => {
-        return item - (indice % 2); // para os índices 0 e 2 , o módulo é 0, para o índice 1, o módulo é 1// o método map() cria um novo array com os resultados da chamada de uma função
-        // O método map() cria um novo array com os resultados da chamada de uma função
-      })
-    );
-    console.log(data);
-
-    // Criando uma instância de Negociacao/ um objeto
     let negociacao = new Negociacao(
-      //this._inputData.value,
-      data,
+      DateConverter.paraData(this._inputData.value),
       parseInt(this._inputQuantidade.value),
       parseFloat(this._inputValor.value)
     );
     console.log(negociacao);
+
+    let diaMesAno = DateConverter.paraTexto(negociacao.data);
+    console.log(diaMesAno);
   }
 }
